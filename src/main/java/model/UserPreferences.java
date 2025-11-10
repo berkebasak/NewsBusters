87e1004d32f34bd4a81ddbb30014bf109ee633c6
Muhammad Zendy Zainuddin <zendyzainuddin425@gmail.com>
2025-11-10 18:07:51 -0500

    Implemented the UserPreferences class.

---
+++ src/main/java/model/UserPreferences.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.util.Set;
+
+public class UserPreferences {
+    private final Set<String> preferredTopics;
+    private final Set<String> blockedSources;
+    private String language;
+    private String region;
+
+    public UserPreferences(Set<String> preferredTopics, Set<String> blockedSources, String language, String region) {
+        this.preferredTopics = preferredTopics;
+        this.blockedSources = blockedSources;
+        this.language = language;
+        this.region = region;
+    }
+
+    public Set<String> getPreferredTopics() {
+        return preferredTopics;
+    }
+
+    public void addPreferredTopic(String newPreferredTopic) {
+        this.preferredTopics.add(newPreferredTopic);
+    }
+
+    public void removePreferredTopic(String preferredTopicToRemove) {
+        this.preferredTopics.remove(preferredTopicToRemove);
+    }
+
+    public Set<String> getBlockedSources() {
+        return blockedSources;
+    }
+
+    public void addBlockedSource(String newBlockedSource) {
+        this.blockedSources.add(newBlockedSource);
+    }
+
+    public void removeBlockedSource(String blockedSourceToRemove) {
+        this.blockedSources.remove(blockedSourceToRemove);
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+}
